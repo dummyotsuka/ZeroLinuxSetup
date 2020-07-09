f2c3dcfe1d77b63f23a1f49288e0335bfbaa036a
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-07-09 16:49:19 +0900

    Update ZeroLinux_setup.sh

--- ZeroLinux_setup.sh
+++ ZeroLinux_setup.sh
@@ -39,6 +39,10 @@ w
 Y
 E
 
+mke2fs  /dev/vda3
+mkdir /lab11
+echo "/dev/vda3 /lab11 ext2 defaults 0 0" >> /etc/fstab
+
 mkdir /tmp/dev/
 dd if=/dev/zero of=/tmp/dev/sda1 bs=1M count=10
 dd if=/dev/zero of=/tmp/dev/sda2 bs=1M count=10
